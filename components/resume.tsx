f0e10f1896ba31f2d3fb17936b9852126d933e2b
Ravi kumar yadav <kumaryadavravi016@gmail.com>
2026-02-21 11:48:05 +0530

    up

--- components/resume.tsx
+++ components/resume.tsx
@@ -32,7 +32,7 @@ export default function Resume() {
             </div>
 
             {/* Buttons */}
-            <div className="space-y-3">              >
+            <div className="space-y-3">              
                 <Button
                   className="w-full bg-blue-600 hover:bg-blue-700"
                   size="lg"
